99283860afcbb40577d541af209114df1161a7ba
Aurionpro <Aurionpro@ACER1208>
2023-09-05 12:05:44 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieAppWithClassLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieController movieController = new MovieController();
+            MovieController.Start();
+        }
+    }
+}
